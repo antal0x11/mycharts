e6d49f7821e193e62835263a4d9a0aedeb3aca5f
Antonios Alexopoulos <el18911@mail.ntua.gr>
2023-05-18 22:45:25 +0300

    add: microservice10 that consumes a message to store the simple plot chart and uses an API to serve it

---
+++ microservice10/README.md
@@ -0,0 +1,8 @@
+# MICROSERVICE10
+
+![ChartsLogo](ChartLogo.png)
+
+## My Charts Simple Plot Storage Server
+
+Microservice10 consists of two parts, the API for myCharts simple plot storage service, and a consumer that consumes messages, from an exchange, that contain the chart, stores it under storage folder and creates a record in a mysql database.
+
---
+++ microservice10/models/chart.js
@@ -0,0 +1,55 @@
+/*
+    Chart Entity.
+*/
+
+
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/db");
+
+const chart = sequelize.define('Chart', {
+        id: {
+            type: DataTypes.STRING,
+            autoIncrement: true,
+            primaryKey: true,
+            allowNull: false
+        },
+
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+
+        users_id: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+
+        chart_id: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+
+        chart_extension: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+
+        chart_type: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+
+        path_chart: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        }
+        
+    }, {
+        tableName : "charts",
+        timestamps: false
+    }
+);
+
+module.exports = chart;
